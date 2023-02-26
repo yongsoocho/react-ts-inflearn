59fea928f07124a1305868b44b88d1bc4516e06f
yongsoocho <yongsoocho578@gmail.com>
2023-02-27 02:25:44 +0900

    1강 기본 틀 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [todo, setTodo] = useState("");
+
+  return (
+    <div className="App">
+      <div style={{ boxSizing: "border-box", padding: "32px" }}>
+        <form
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            style={{ marginRight: "16px" }}
+          />
+          <button type="submit">추가</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
